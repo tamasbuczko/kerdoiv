52d2d55af92314e4fb3f78229ffccd4a20c5acd8
Molnár Zoltán <molnarzoli82@gmail.com>
2014-01-01 21:56:24 +0100

    - figyelmeztetés kikapcsolása első oldallátogatáskor

--- public/tartalomvalasztas.php
+++ public/tartalomvalasztas.php
@@ -243,19 +243,22 @@ if ($hiba > 0){
 //nem lett kijelölve
 
 if ($figyelmeztetes > 0){
-    foreach($kerdesek as $key => $value){
-       if ($kerdesek[$key] == '0'){ 
-	   $figy_uzenet .= $key.', ';
-	   }
-    }
-	if ($figy_uzenet){
-	  $figy_uzenet = substr($figy_uzenet, 0, -2);
-	  $figy_uzenet = '<h3><br />'.$lang['nem_valaszoltal'].'</h3>'.$figy_uzenet;
-	}
+   if ($_REQUEST[submit]){
+	  foreach($kerdesek as $key => $value){
+		 if ($kerdesek[$key] == '0'){ 
+		 $figy_uzenet .= $key.', ';
+		 }
+	  }
+	  if ($figy_uzenet){
+		$figy_uzenet = substr($figy_uzenet, 0, -2);
+		$figy_uzenet = '<h3><br />'.$lang['nem_valaszoltal'].'</h3>'.$figy_uzenet;
+	  }
+   }
 }
 else {
    $_REQUEST['b'] = '1';
 }
+
 if (!$figy_uzenet){
     $_REQUEST['b'] = '1';
 }
