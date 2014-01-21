d1b8c61f38e92dbb21131cad70d257ce565df351
BuczkoT <BuczkoT@nbk011.olajterv.hu>
2014-01-21 21:09:48 +0100

    - kiértékelés diagrammal

--- public/eredmeny.php
+++ public/eredmeny.php
@@ -12,27 +12,55 @@ while ($next_element = mysql_fetch_array($result)){
     $sorszam_kerdes = $next_element[sorszam];
     $kerdes_tipus = $next_element[tipus]; // aktuális kérdés tipusa
     
-    if (($kerdes_tipus = 'radio') OR ($kerdes_tipus = 'select') OR ($kerdes_tipus = 'checkbox')){
+    if (($kerdes_tipus == 'radio') OR ($kerdes_tipus == 'select') OR ($kerdes_tipus == 'checkbox')){
+        $result2 = mysql_query("SELECT sorszam FROM valaszadasok WHERE kerdes_sorszam = $sorszam_kerdes");
+        $valaszadok_szama = mysql_num_rows($result2);
+        
         $result2 = mysql_query("SELECT k.sorszam, k.kerdes_hu, v.valasz_hu, COUNT(*)
         FROM valaszadasok AS va
         LEFT JOIN valaszok AS v ON va.valasz_sorszam = v.sorszam
         LEFT JOIN kerdesek AS k ON va.kerdes_sorszam = k.sorszam
         WHERE k.sorszam = $sorszam_kerdes
         GROUP BY valasz_hu
-        ORDER BY k.sorszam");
+        ORDER BY COUNT(*) DESC");
 
         while ($eredmenyek = mysql_fetch_array($result2)){
-            $eredmeny_lista1 .= $eredmenyek[2].' ('.$eredmenyek[3].')<br />';
+            $eredmenyarany = $eredmenyek[3] / $valaszadok_szama;
+            $eredmenyarany = $eredmenyarany*200;
+            $eredmeny_lista .= $eredmenyek[2].' ('.$eredmenyek[3].') <div class="graf" style="width: '.$eredmenyarany.'px"></div><br />';
         }
     }
     
-    if (($kerdes_tipus = 'text') OR ($kerdes_tipus = 'textarea')){
+    if ($kerdes_tipus == 'ranking'){
+        $result2 = mysql_query("SELECT k.sorszam, k.kerdes_hu, v.valasz_hu, va.ertek, COUNT(*)
+        FROM valaszadasok AS va
+        LEFT JOIN valaszok AS v ON va.valasz_sorszam = v.sorszam
+        LEFT JOIN kerdesek AS k ON va.kerdes_sorszam = k.sorszam
+        WHERE k.sorszam = $sorszam_kerdes
+        GROUP BY valasz_hu, va.ertek
+        ORDER BY v.sorszam, va.ertek");
+        
+        
+        while ($eredmenyek = mysql_fetch_array($result2)){
+            if ($rank_archiv == $eredmenyek[2]){
+                $rank_valasz = '';
+                $sortores = '';
+            } else {
+                $rank_valasz = $eredmenyek[2];
+                $sortores = '<br />';
+            }
+            $rank_archiv = $eredmenyek[2];
+            $eredmeny_lista .= $sortores.$rank_valasz.' - '.$eredmenyek[3].' ('.$eredmenyek[4].')';
+            
+        }
+    }
+    
+    if (($kerdes_tipus == 'text') OR ($kerdes_tipus == 'textarea')){
         $result2 = mysql_query("SELECT k.sorszam, k.kerdes_hu, v.valasz_hu, va.szoveg
         FROM valaszadasok AS va
         LEFT JOIN valaszok AS v ON va.valasz_sorszam = v.sorszam
         LEFT JOIN kerdesek AS k ON va.kerdes_sorszam = k.sorszam
         WHERE k.sorszam = $sorszam_kerdes
-        GROUP BY valasz_hu
         ORDER BY k.sorszam");
 
         while ($eredmenyek = mysql_fetch_array($result2)){
