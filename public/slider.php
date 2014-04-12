e85c39c7b90e573acdd7459268e829232b43a4b8
Molnár Zoltán <molnarzoli82@gmail.com>
2014-04-12 08:57:11 +0200

    - slider képek

--- public/slider.php
+++ public/slider.php
@@ -3,21 +3,21 @@
 $slider =
 			'<ul id="slider">
 			   <li>
-				  <img src="slider/1.jpg" alt="" />
+				  <img src="slider/1.png" alt="" />
 				  <div class="rhino-caption">
-					 <div class="rhino-white">Minőségi nyomtatás, minőségi anyagokra</div>
+					 <div class="rhino-white"> </div>
 				  </div>
 			   </li>
 			   <li>
-				  <img src="slider/2.jpg" alt="" />
+				  <img src="slider/2.png" alt="" />
 				  <div class="rhino-caption">
-					 <div class="rhino-white">Minőségi nyomtatás, minőségi anyagokra</div>
+					 <div class="rhino-white"> </div>
 				  </div>
 			   </li>
 			   <li>
-				  <img src="slider/3.jpg" alt="" />
+				  <img src="slider/3.png" alt="" />
 				  <div class="rhino-caption">
-					 <div class="rhino-white">Minőségi nyomtatás, minőségi anyagokra</div>
+					 <div class="rhino-white"> </div>
 				  </div>
 			   </li>
 			</ul>';
