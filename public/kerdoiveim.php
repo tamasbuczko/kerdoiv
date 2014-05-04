71a59b979d354d3109dffca72a8d99212e6e5d82
Molnár Zoltán <molnarzoli82@gmail.com>
2014-05-04 19:38:16 +0200

    - kérdőíveim lapon aktív/inaktív kapcsoló
    fixes #2

--- public/kerdoiveim.php
+++ public/kerdoiveim.php
@@ -1,13 +1,17 @@
 <?php
+if ($_REQUEST[id]){
+   $result = mysql_query("UPDATE kerdoivek SET status = '$_REQUEST[status]' WHERE sorszam = $_REQUEST[id]");
+   mysql_query($result);
+   header("Location: ?p=7");
+}
 
-
-$result = mysql_query ("SELECT sorszam, cim_hu, cim_en, cim_de FROM kerdoivek WHERE status = '1' AND user_id = '$_SESSION[qa_user_id]' ");
+$result = mysql_query ("SELECT sorszam, cim_hu, cim_en, cim_de, status FROM kerdoivek WHERE user_id = '$_SESSION[qa_user_id]' ");
 while ($next_element = mysql_fetch_array($result)){
    
    $result2 = mysql_query("SELECT sorszam FROM valaszadasok WHERE kerdoiv_sorszam = $next_element[sorszam] GROUP BY kitolto_sorszam");
    $valaszadok_szama = mysql_num_rows($result2);
    
-   $resultc = mysql_query ("SELECT cim_hu, cim_en, cim_de, leiras_hu, leiras_en, leiras_de, hu, en, de FROM kerdoivek WHERE status = '1' AND sorszam = '$next_element[sorszam]' ");
+   $resultc = mysql_query ("SELECT cim_hu, cim_en, cim_de, leiras_hu, leiras_en, leiras_de, hu, en, de FROM kerdoivek WHERE sorszam = '$next_element[sorszam]' ");
    $next_elementc = mysql_fetch_array ($resultc);
    $kerdoiv_cim=$next_elementc['cim_'.$_SESSION[lang]];
    $kerdoiv_leiras=$next_elementc['leiras_'.$_SESSION[lang]];
@@ -30,7 +34,7 @@ while ($next_element = mysql_fetch_array($result)){
    
    if ($nyelv_db > 1){
 	  $nyelv_fejlec = '<th>nyelvek</th>';
-	  $zaszlok = '<td class="kis_zaszlo">'.$zaszlo_en.$zaszlo_de.$zaszlo_hu.'</td>';
+	  $zaszlok = $zaszlo_en.$zaszlo_de.$zaszlo_hu;
    } else {
 	  $zaszlok = '';
    }
@@ -43,11 +47,22 @@ while ($next_element = mysql_fetch_array($result)){
 	   if ($cim == ''){ $cim = $next_element[cim_de];}
 	}
 	
-    $lista_kerdoiveim .= '<tr><td><a href="?p=kerdoiv_adatlap&kerdoiv='.$next_element[sorszam].'">'.$cim.'</a></td>'
+	if ($next_element[status] == 1){
+	   $status = '<a href="?p=7&amp;id='.$next_element[sorszam].'&amp;status=0"><img src="graphics/active.jpg" alt="kikapcsol" title="kikapcsol" /></a>';
+	}
+	if ($next_element[status] == 0){
+	   $status = '<a href="?p=7&amp;id='.$next_element[sorszam].'&amp;status=1"><img src="graphics/inactive.jpg" alt="bekapcsol" title="bekapcsol" /></a>';
+	}
+	
+    $lista_kerdoiveim .= '<tr>'
+			. '<td><a href="?p=kerdoiv_adatlap&kerdoiv='.$next_element[sorszam].'">'.$cim.'</a></td>'
 			. '<td><a href="?p=eredmeny&kerdoiv='.$next_element[sorszam].'"><img src="graphics/icon_graph.png" alt="eredmények" /></a></td>'
 			. '<td><a href="?p=kerdoiv&kerdoiv='.$next_element[sorszam].'"><img src="graphics/icon_checked.png" alt="eredmények" /></a></td>'
 			. '<td><a href="?p=kerdoiv&amp;mod=1&amp;kerdoiv='.$next_element[sorszam].'"><img src="graphics/icon_edit.gif" alt="eredmények" /></a></td>'
-			. '<td><img src="graphics/inactive.jpg" alt="aktiválás" /></td><td>'.$valaszadok_szama.' fő</td>'.$zaszlok.'</tr>';
+			. '<td>'.$status.'</td>'
+			. '<td>'.$valaszadok_szama.' fő</td>'
+			. '<td class="kis_zaszlo">'.$zaszlok.'</td>'
+			. '</tr>';
 }
 
 
