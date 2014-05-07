14a7f7ae4e09755a256a4e0de7f837e47d55730b
Molnár Zoltán <molnarzoli82@gmail.com>
2014-05-07 22:59:24 +0200

    - sorsolási lehetőség a kitöltők email címeiből (a dizájnt majd megbeszéljük)
    fixes #8

--- public/kerdoiv_kitoltoemail.php
+++ public/kerdoiv_kitoltoemail.php
@@ -4,8 +4,37 @@ $result = mysql_query("SELECT va.sorszam, k.email AS email FROM valaszadasok AS 
 $sor = 0;
 while ($next_element = mysql_fetch_array($result)){
    $sor++;
-   $list .= $sor.'. '.$next_element[email].'<br />';
+   $list .= $next_element[email].'<br />';
    $emailek[$sor] = $next_element[email];
 }
 
-$tartalom = $list;
+$sorsol_db= $_REQUEST[sorsol_db];
+
+for ($i = 1; $i<=$sorsol_db; $i++){
+   $veletlen = rand(1,$sor);
+   $nyertesek .= $i.'. ' . $emailek[$veletlen]. ' <br />';
+   unset($emailek[$veletlen]);
+}
+
+if ($sorsol_db > 1){
+   $tobbes = 'nyertesek';
+} else {
+   $tobbes = 'nyertes';
+}
+
+if ($nyertesek){
+   $nyertesek = ''
+		. '<div>'
+		. '<h2>A '.$tobbes.':</h2>'
+		. $nyertesek
+		. '<br /><br />'
+		. '</div>';
+}
+
+$form = '<form action="" method="post">'
+		. '<label>Hány nyertest akarsz sorsolni?</label><br />'
+		. '<input type="text" name="sorsol_db" value="" style="width: 50px;" /><br />'
+		. '<input type="submit" value="Sorsolás" />'
+		. '</form>';
+
+$tartalom = $form . $nyertesek . $list;
