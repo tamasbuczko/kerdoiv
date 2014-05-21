d607ec66db6f1ec00a461ec976765ba299c7da66
Molnár Zoltán <molnarzoli82@gmail.com>
2014-05-21 18:11:43 +0200

    - Google Analytics követőkód beágyazása (meglévő fiókoddal eléred)

--- js/ganalytics.js
+++ js/ganalytics.js
@@ -3,5 +3,5 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-41887723-9', 'questionaction.com');
+  ga('create', 'UA-51208674-1', 'questionaction.com');
   ga('send', 'pageview');
