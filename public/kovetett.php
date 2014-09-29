685ee9c181bcd5750461f91c2de4f433c3fb78f5
Molnár Zoltán <molnarzoli82@gmail.com>
2014-09-29 10:22:47 +0200

    - címlapon a követett kérdőívek nyelvhelyes megjelenése
    fixes #182

--- public/kovetett.php
+++ public/kovetett.php
@@ -1,13 +1,12 @@
 <?php
 if ($_REQUEST[keres]){
    $keres_szur = "AND (cim_hu LIKE '%$_REQUEST[keres]%' OR cim_en LIKE '%$_REQUEST[keres]%' OR cim_de LIKE '%$_REQUEST[keres]%'
-
            OR leiras_hu LIKE '%$_REQUEST[keres]%' OR leiras_en LIKE '%$_REQUEST[keres]%' OR leiras_de LIKE '%$_REQUEST[keres]%')";
 }
 
-if ($_SESSION[lang] == 'hu'){ $nyelv_szur = "AND hu = '1'";};
-if ($_SESSION[lang] == 'en'){ $nyelv_szur = "AND en = '1'";};
-if ($_SESSION[lang] == 'de'){ $nyelv_szur = "AND de = '1'";};
+if ($_SESSION[lang] == 'hu'){ $nyelv_szur = "AND ki.hu = '1'";};
+if ($_SESSION[lang] == 'en'){ $nyelv_szur = "AND ki.en = '1'";};
+if ($_SESSION[lang] == 'de'){ $nyelv_szur = "AND ki.de = '1'";};
 
 $result = "SELECT DISTINCT va.kerdoiv_sorszam, ki.sorszam, ki.cim_hu, ki.cim_de, ki.cim_en FROM valaszadasok AS va
         LEFT JOIN kitoltok AS k 
