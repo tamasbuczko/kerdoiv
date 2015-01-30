61fa4418c81d6ddda4ce798af63cdadd3d4c164a
Tamás <Tams@Tams-PC>
2015-01-30 22:35:17 +0100

    - e-mail lista másolása a választott kérdőív zárt rendszerébe

--- public/kitoltok_valaszai.php
+++ public/kitoltok_valaszai.php
@@ -3,22 +3,18 @@ if ($_SESSION[qa_user_id] == $kerdoiv_obj->keszito_id){
 
 $kerdoiv= $_REQUEST[kerdoiv];
 $status = 1;
-if ($_REQUEST[masol]){ //Kell egy ellenőrzés, hogy csak egyszer lehessen ezt az adatbázisba másolni. 
+if ($_REQUEST[masol]){ //Kell egy ellenőrzés, hogy csak egyszer lehessen ezt az adatbázisba másolni!!!!!!!!!!!!!!!!!!!!!!!
     $result2 = mysql_query("SELECT DISTINCT va.kitolto_sorszam, k.email FROM valaszadasok AS va
                        LEFT JOIN kitoltok AS k ON va.kitolto_sorszam = k.sorszam
-                       WHERE va.kerdoiv_sorszam = '$kerdoiv'");
+                       WHERE va.kerdoiv_sorszam = '$kerdoiv' AND k.email != ''");
 while ($row = mysql_fetch_array($result2)){
     $email_cim = $row[email];
 
     $sql = "INSERT INTO zart_emailek (email, kerdoiv, status) 
-           VALUES ('$email_cim', '$kerdoiv', '$status')";
+           VALUES ('$email_cim', '$_REQUEST[kerdoiv_szam]', '$status')";
     mysql_query($sql); //futtatás   
 }
 }
-
-$zart_rendszerbe_masol = '<form method="post" action="" style="margin-bottom:20px;">				
-			<input type="submit" name="masol" value="E-mailek másolása a zárt rendszerbe">
-                        </form>';
 
 $sorok = 0;
 $result = mysql_query("SELECT DISTINCT va.kitolto_sorszam, k.email FROM valaszadasok AS va
@@ -28,5 +24,18 @@ while ($row = mysql_fetch_array($result)){
     $sorok++;
     $lista .= '<a href="?p=kerdoiv&kerdoiv='.$kerdoiv.'&er=1&kitolto='.$row[kitolto_sorszam].'">'.$sorok.'. '.$row[email].'</a><br />';
 }
+
+$result3 = mysql_query("SELECT sorszam, cim_hu, cim_de, cim_en FROM kerdoivek WHERE user_id = $_SESSION[qa_user_id]");
+        while ($a = mysql_fetch_array($result3)){
+            $kerdoiv_tomb[$a['sorszam']] = $a['cim_'.$_SESSION[lang]];
+        }
+
+$kitolto_lista='Kitöltők válaszai: <br />' .$lista;
+
+$smarty->assign('kitolto_lista',$kitolto_lista);
+$smarty->assign('kerdoiv_tomb', $kerdoiv_tomb);  
+$smarty->assign('kerdoiv_obj', $kerdoiv_obj);
+
+$tartalom = $smarty->fetch('templates/kitoltok_valaszai.tpl');
+
 }
-$tartalom='Kitöltők válaszai: <br />' .$zart_rendszerbe_masol. $lista;
---
+++ templates/kitoltok_valaszai.tpl
@@ -0,0 +1,11 @@
+<form method="post" action="" style="margin-bottom:20px;">
+<label>Kérdőíveim:</label><br />
+    <select name="kerdoiv_szam" class="search" style="width:400px; margin-left:1px !important; font-size:12px; height: 29px; padding-top: 2px;">
+    <option value="">{$kerdoiv_obj->cim}</option>
+{foreach from=$kerdoiv_tomb key="kerdoiv_szam" item="kerdoiv_cim"}
+    <option value="{$kerdoiv_szam}">{$kerdoiv_cim}</option>
+{/foreach}
+    </select> 
+    <input type="submit" name="masol" value="E-mailek másolása a választott kérdőív zárt rendszerébe" style="margin-left: 5px; width: 350px; height: 29px;">
+</form>
+{$kitolto_lista}
