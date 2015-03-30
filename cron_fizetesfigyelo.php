9da824713a90b9dcb7afa2dfa5cf119579016746
Molnár Zoltán <molnarzoli82@gmail.com>
2015-03-30 12:44:10 +0200

    -  csomagfizetés napi ellenőrzésének előkészítése (cron)

---
+++ cron_fizetesfigyelo.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+//néhány rendszerállandó beállítása
+require_once('parameters.php');
+
+//osztályok betöltése az objektumokhoz
+require_once('class/class.php');
+
+//kapcsolat létrehozása az adatbázis szerverrel (class.php)
+$adatkapcsolatx = new data_connect;
+$adatkapcsolatx->connect();
+
+//minden nap éjjel lefut
+
+//ha a felhasználó fizetős csomagban van és fizetve van az aktuális időszaka, akkor létrehozza a következő
+//időszakának fizetési lehetőségét
+
+//értesítést küld a lejárat előtt 5 nappal a hosszabbítási lehetőségről
+
+//értesítést küld a lejárat napján a hosszabbítási lehetőségről
+
+//értesítést küld a lejáratot követő napokban (5 napon keresztül) a hosszabbítási lehetőségről
+
+//lejáratot követő 6. napon ingyenes csomagra vált és értesíti a felhasználót
